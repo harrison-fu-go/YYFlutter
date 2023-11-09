59ee692172e22c716572aeb6e0c15e2e47e87d49
harrison.fu <146048040+harrison-fu-go@users.noreply.github.com>
2023-11-09 17:14:55 +0800

    Delete flutter_lib_research/FlutterLibPodDemo/Pods directory

--- flutter_lib_research/FlutterLibPodDemo/Pods/Target Support Files/DemoLib/DemoLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DemoLib : NSObject
-@end
-@implementation PodsDummy_DemoLib
-@end
--- flutter_lib_research/FlutterLibPodDemo/Pods/Target Support Files/Pods-FlutterLibPodDemo/Pods-FlutterLibPodDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlutterLibPodDemo : NSObject
-@end
-@implementation PodsDummy_Pods_FlutterLibPodDemo
-@end
